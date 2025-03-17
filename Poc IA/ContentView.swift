ce4a9cfb57133135064831919b4794a7224f7dd0
Sergio Cesar <sergiocesar0203@gmail.com>
2025-03-17 13:32:56 -0400

    my first commit

---
+++ Poc IA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Poc IA
+//
+//  Created by Sérgio César Lira Júnior on 17/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Poc IA/Poc_IAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poc_IAApp.swift
+//  Poc IA
+//
+//  Created by Sérgio César Lira Júnior on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Poc_IAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
